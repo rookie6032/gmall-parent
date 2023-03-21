d19f6dd15543cb28b897b7c6e7341e301468abd3
gxf <105686073+rookie6032@users.noreply.github.com>
2023-03-21 10:17:09 +0800

    Changes

---
+++ service/web-front/src/test/java/com/atguigu/test/StringTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.test;
+
+public class StringTest {
+    public static void main(String[] args) {
+//        String s1 = "Runoob";              // String 直接创建
+//        String s2 = "Runoob";
+//        System.out.println(s1==s2);
+//        String s3 = s1;
+//        System.out.println(s1==s3);
+
+        Test2 ac = new Test2();
+        ac.test2();
+    }
+}
---
+++ service/web-front/src/test/java/com/atguigu/test/Test2.java
@@ -0,0 +1,9 @@
+package com.atguigu.test;
+
+public class Test2 {
+
+    @Deprecated
+    public void test2(){
+        System.out.println("hello");
+    }
+}
